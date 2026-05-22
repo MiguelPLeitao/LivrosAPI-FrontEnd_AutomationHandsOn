b58d5b666a5de82ea0de7a267ccafe39bdd80bce
MiguelPLeitao <miguel.p.leitao@sapo.pt>
2026-05-22 21:18:46 +0100

    added Allure Report to GitHubActions

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,10 @@ export default defineConfig({
   workers: 1,
   //process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
